a3348242f852eeebe5313d4bf3eadb948f1db1b2
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-12 21:59:08 -0300

    apply masks

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,15 +5,36 @@ import { styles } from "./styles"
 
 import { Input } from "@/components/Input"
 
+import { cnpjApplyMask, cpfApplyMask } from "@/utils/masks"
+
 export function Home() {
   const [pix, setPix] = useState("")
+
+  function applyMask(input: string) {
+    const onlyNumbers = input.replace(/[^\d]/g, "")
+
+    if (onlyNumbers.length === 11) {
+      const cpf = cpfApplyMask(onlyNumbers)
+
+      return setPix(cpf)
+    }
+
+    if (onlyNumbers.length === 14) {
+      const cnpj = cnpjApplyMask(onlyNumbers)
+
+      return setPix(cnpj)
+    }
+
+    const nameOrPixKey = input.replace(/[^a-zA-Z0-9 ]/g, "")
+    setPix(nameOrPixKey)
+  }
 
   return (
     <View style={styles.container}>
       <Input.Container>
         <Input.Field
           placeholder="Nome, CPF/CNPJ ou chave Pix"
-          onChangeText={setPix}
+          onChangeText={applyMask}
           value={pix}
         />
 
